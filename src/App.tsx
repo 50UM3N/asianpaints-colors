5ad29d8e92c97fd8c4c515700ed3df20901421a8
Soumen Khara <soumen2015.s.k@gmail.com>
2024-04-22 17:09:30 +0530

    feat: added stuff

--- src/App.tsx
+++ src/App.tsx
@@ -19,17 +19,6 @@ function rgbToHex(rgbColor: string): string {
   return `#${rHex}${gHex}${bHex}`;
 }
 function App() {
-  const [copied, setCopied] = useState(false);
-
-  const handleCopy = async (value: string) => {
-    try {
-      await navigator.clipboard.writeText(value);
-      setCopied(true);
-      setTimeout(() => setCopied(false), 2000); // Reset copied status after 2 seconds
-    } catch (err) {
-      console.error("Failed to copy text: ", err);
-    }
-  };
   return (
     <div className="py-12 px-8">
       {colors.map((color, index) => (
@@ -40,19 +29,17 @@ function App() {
                 className="aspect-[2/1] w-full flex justify-center items-center group"
                 style={{ background: shade.color }}
               >
-                <div
-                  onClick={() => handleCopy(rgbToHex(shade.color))}
+                <a
+                  target="_blank"
+                  href={shade.url}
                   className="bg-white rounded-full px-2 text-xs cursor-pointer opacity-0 group-hover:opacity-100 transition-all ease-in-out duration-150 select-none"
                 >
-                  {copied ? <span className="text-green-500">Copied!</span> : rgbToHex(shade.color)}
-                  {}
-                </div>
+                  view
+                </a>
               </div>
               <div className="flex justify-between pt-1">
-                <p className="text-xs">{shade.shadecode}</p>
-                <a className="text-xs" href={shade.url} target="_blank">
-                  View
-                </a>
+                <CopyItem  value={shade.shadecode} />
+                <CopyItem value={rgbToHex(shade.color)} />
               </div>
             </div>
           ))}
@@ -63,3 +50,22 @@ function App() {
 }
 
 export default App;
+
+const CopyItem: React.FC<{ value: string }> = ({ value, ...rest }) => {
+  const [copied, setCopied] = useState(false);
+
+  const handleCopy = async () => {
+    try {
+      await navigator.clipboard.writeText(value);
+      setCopied(true);
+      setTimeout(() => setCopied(false), 2000); // Reset copied status after 2 seconds
+    } catch (err) {
+      console.error("Failed to copy text: ", err);
+    }
+  };
+  return (
+    <p className="text-[10px] cursor-pointer select-none" {...rest} onClick={handleCopy}>
+      {copied ? <span className="text-green-500">Copied!</span> : value}
+    </p>
+  );
+};
